00f17b4e8899aa8b18d12674eb03587b7e045c34
Sam Kirtley <kirtley.sam@gmail.com>
2014-08-10 18:17:36 -0400

    Homepage given its own template

--- wp-content/themes/deskjockeygm/index.php
+++ wp-content/themes/deskjockeygm/index.php
@@ -1,30 +1,16 @@
 <?php get_header(); ?>
 
-<div class="content-wrap">
-	<div class="left-side">
-		<?php 
-		if (have_posts()) :
-		   while (have_posts()) :
-		      the_post(); ?>
-		    <?php if (($wp_query->current_post) % 2) $class="evenPost"; else $class="oddPost"; ?>
-		  	<div <?php post_class($class); ?> <?php if( $wp_query->current_post == 0 && !is_paged() ) { echo 'id="first"'; } ?>>
-				<div class="feature-image"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail( 'large' ); ?></a></div>
-				<div class="text-wrap">
-					<h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-			        <?php the_excerpt(); ?>
-			        <div class="read-more"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">Read More</a></div>
-		    	</div><!--text-wrap-->
-		    </div><!--single-post-->
-		   <?php endwhile;
-		endif; ?>
-	</div><!--left-side-->
+<body <?php body_class($class); ?>>
 
-	<div class="right-side">
-		<div class="content">
-			<?php fetchTweets( array( 'screen_name' => 'samkirtley',  'count' => 10 ) ); ?>
-		</div><!--content-->
-	</div><!--right-side-->
-	<div class="clear"></div>
-</div><!--content-wrap-->
+<?php 
+if (have_posts()) :
+   while (have_posts()) :
+      the_post();
+         the_content();
+   endwhile;
+endif;
+?>
+<?php edit_post_link(); ?>
+
 
 <?php get_footer(); ?>
