18e7e44d0f8246f99c27845e4ddadbbcbc576648
Sam Kirtley <kirtley.sam@gmail.com>
2014-08-09 22:31:46 -0400

    Bunch of homepage development

--- wp-content/themes/deskjockeygm/footer.php
+++ wp-content/themes/deskjockeygm/footer.php
@@ -1,5 +1,7 @@
 <footer>
-<div>Sams Footer</div>
+<div>Desk Jockey General Manager &copy; <?php echo date("Y") ?>. All rights reserved.</div>
+Follow us on twitter.
+Menu to about page?
 <?php wp_footer(); ?>
 </footer>
 </div><!--main-wrap-->
--- wp-content/themes/deskjockeygm/index.php
+++ wp-content/themes/deskjockeygm/index.php
@@ -1,14 +1,29 @@
 <?php get_header(); ?>
 
-<?php 
-if (have_posts()) :
-   while (have_posts()) :
-      the_post();
-         the_content();
-   endwhile;
-endif;
-?>
-<?php edit_post_link(); ?>
+<div class="content-wrap">
+	<div class="left-side">
+		<?php 
+		if (have_posts()) :
+		   while (have_posts()) :
+		      the_post(); ?>
+		    <?php if (($wp_query->current_post) % 2) $class="evenPost"; else $class="oddPost"; ?>
+		  	<div <?php post_class($class); ?> <?php if( $wp_query->current_post == 0 && !is_paged() ) { echo 'id="first"'; } ?>>
+				<div class="feature-image"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail( 'large' ); ?></a></div>
+				<div class="text-wrap">
+					<h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+			        <?php the_excerpt(); ?>
+			        <div class="read-more"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">Read More</a></div>
+		    	</div><!--text-wrap-->
+		    </div><!--single-post-->
+		   <?php endwhile;
+		endif; ?>
+		<?php edit_post_link(); ?>
+	</div><!--left-side-->
 
+	<div class="right-side">
+		Right side content...Twitter Feed
+	</div><!--right-side-->
+	<div class="clear"></div>
+</div><!--content-wrap-->
 
 <?php get_footer(); ?>
