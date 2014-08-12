522f367f73c172dd6ac9b773a48a82c66716eafc
Sam Kirtley <kirtley.sam@gmail.com>
2014-08-11 20:49:47 -0400

    Category page created, wordpress add ons

--- wp-content/themes/deskjockeygm/index.php
+++ wp-content/themes/deskjockeygm/index.php
@@ -1,16 +1,17 @@
 <?php get_header(); ?>
 
 <body <?php body_class($class); ?>>
-
-<?php 
-if (have_posts()) :
-   while (have_posts()) :
-      the_post();
-         the_content();
-   endwhile;
-endif;
-?>
-<?php edit_post_link(); ?>
+	<div class="body-content">
+		<?php 
+		if (have_posts()) :
+		   while (have_posts()) :
+		      the_post(); ?>
+		         <h2><?php the_title(); ?></h2>
+		         <?php the_content(); ?>
+		   <?php endwhile;
+		endif; ?>
+		<?php edit_post_link(); ?>
+	</div><!--body_content-->
 
 
 <?php get_footer(); ?>
